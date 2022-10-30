6a8256bacaec9d2bd460fb437c11ed17976510f4
VeeNationWide <102980506+VeeNationWide@users.noreply.github.com>
2022-10-30 22:26:02 +0200

    Add project files.

---
+++ BikeStoreSystem/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BikeStoreSystem.Models;
+
+namespace BikeStoreSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private BikeStoresEntities db = new BikeStoresEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Chart()
+        {
+            return View();
+        }
+    }
+}
---
+++ BikeStoreSystem/Controllers/ordersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using BikeStoreSystem.Models;
+using PagedList.Mvc;
+using PagedList;
+using System.Data;
+using System.Data.Entity;
+
+
+namespace BikeStoreSystem.Controllers
+{
+    public class ordersController : Controller
+    {
+
+
+
+        private BikeStoresEntities db = new BikeStoresEntities();
+
+        public ActionResult Index(int? i)
+        {
+            var Orders = db.order_items.Include(n => n.product).Include(n=>n.order);
+            return View(Orders.ToList().ToPagedList(i ?? 1, 10));
+        }
+    }
+}
